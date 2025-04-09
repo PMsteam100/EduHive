acc3bbcf23dbae9a9b782169861e005cb249ed3e
Adeyinka Adebayo A <206746647+PMsteam100@users.noreply.github.com>
2025-04-09 18:12:42 +0100

    try

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// import path from "path"
+// import tailwindcss from "@tailwindcss/vite"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: "/"
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/"
-})
+  plugins: [react(),tailwindcss()],
+  server: {
+    historyApiFallback: true,
+  },
+});
