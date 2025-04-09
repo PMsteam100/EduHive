bd824e612070e1d53dd76ee3fb19763db15bfa1e
Adeyinka Adebayo A <206746647+PMsteam100@users.noreply.github.com>
2025-04-09 15:56:57 +0100

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 import { HashRouter, Routes, Route } from 'react-router-dom';
+
 import Login from './AloginRegister/Login';
 
 createRoot(document.getElementById('root')).render(
@@ -12,10 +13,6 @@ createRoot(document.getElementById('root')).render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/Login" element={<Login />} />
-      {/* <Route path="/Main_team" element={<Main_team />} />
-      <Route path="/Main_support" element={<Main_support />} />
-      <Route path="/Sign_in" element={<Sign_in />} />
-      <Route path="*" element={<Error />} /> */}
     </Routes>
   </HashRouter>
 </StrictMode>,
